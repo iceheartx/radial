dce376647e90ca8ce9a5ea52f6085fbb3ff50768
iceheartx <github@iceheart.org>
2016-10-22 23:37:31 -0700

    readme, added non-function PUI colors and form controls, initial form control componants

--- app/components/application.js
+++ app/components/application.js
@@ -7,41 +7,100 @@ const {useRouter} = require('./use_router');
 const Router = require('./router');
 
 class Application extends React.Component {
-  static propTypes = {
-    config: types.object.isRequired,
-    store: types.object.isRequired,
-    router: types.oneOfType([types.object, types.func])
-  };
+    static propTypes = {
+        config: types.object.isRequired,
+        store: types.object.isRequired,
+        router: types.oneOfType([types.object, types.func])
+    };
 
-  render() {
-    const {config, store, router} = this.props;
-    return (
-      <div className="pui-react-starter">
-        <a href="todoList" onClick={e => {e.preventDefault(); router.navigate('/todoList');}}>Todo List!</a>
-        <br/>
-        <a href="apiPage" onClick={e => {e.preventDefault(); router.navigate('/apiPage');}}>Page that hits an api</a>
-        <br/>
-        <a href="createNewUser" onClick={e => {e.preventDefault(); router.navigate('/users/new');}}>Create New User</a>
-        <br/>
-        <a href="userList" onClick={e => {e.preventDefault(); router.navigate('/users/list');}}> All Users</a>
-        <Router {...{router, config, ...store}}/>
-      </div>
-    );
-  }
+    render() {
+        const {config, store, router} = this.props;
+        return (
+            <div className="app">
+            <div className="controls">
+                <form role="form">
+                    <h1>SVG</h1>
+                    <div className="form-group">
+                        <label>
+                            <input type="number" className="form-control" id="svgSize"/>
+                            SVG Size</label>
+                    </div>
+                    <div className="form-group">
+                        <label>
+                            <input type="number" className="form-control" id="svgCenterX"/>
+                            SVG Center X
+                        </label>
+                    </div>
+                    <div className="form-group">
+                        <label>
+                            <input type="number" className="form-control" id="svgCenterY"/>
+                            SVG Center Y
+                        </label>
+                    </div>
+                    <div className="form-group">
+                        <label>
+                            <input type="number" className="form-control" id="svgMargin"/>
+                            SVG Margin
+                        </label>
+                    </div>
+                    <div className="form-group">
+                        <label>
+                            <input type="text" className="form-control" id="svgBgcolor"/>
+                            SVG Background Color
+                        </label>
+                    </div>
+                    <h1>Buttons</h1>
+                    <div className="form-group">
+                        <label>
+                            <input type="text" className="form-control" id="buttonBgcolor"/>
+                            Button Background Color
+                        </label>
+                    </div>
+                    <div className="form-group">
+                        <label>
+                            <input type="text" className="form-control" id="buttonBorderColor"/>
+                            Button Border Color
+                        </label>
+                    </div>
+                    <div className="form-group">
+                        <label>
+                            <input type="text" className="form-control" id="buttonHoverColor"/>
+                            Button Hover Color
+                        </label>
+                    </div>
+                    <h1>Icons</h1>
+                    <div className="form-group">
+                        <label>
+                            <input type="text" className="form-control" id="iconsFillColor"/>
+                            Icons Fill Color
+                        </label>
+                    </div>
+
+                    <button type="submit" className="btn-default">Draw</button>
+                </form>
+            </div>
+                <div className="radialHolder">
+I contain multitudes;
+                </div>
+                </div>
+        );
+    }
 }
 
 const EnhancedApplication = useStore(useRouter(Application),
-  {
-    store: require('../store'),
-    actions: [],
-    dispatcherHandlers: [
-      require('../dispatchers/main_dispatcher'),
-      require('../dispatchers/api_dispatcher')
-    ],
-    /* eslint-disable no-console */
-    onDispatch: (event) => {console.info('dispatching event', event);}
-    /* eslint-enable no-console */
-  }
+    {
+        store: require('../store'),
+        actions: [],
+        dispatcherHandlers: [
+            require('../dispatchers/main_dispatcher'),
+            require('../dispatchers/api_dispatcher')
+        ],
+        /* eslint-disable no-console */
+        onDispatch: (event) => {
+            console.info('dispatching event', event);
+        }
+        /* eslint-enable no-console */
+    }
 );
 
 Bootstrap.init(EnhancedApplication);
